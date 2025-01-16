c1cafec138fbba13f291f6e2db715e83196b534e
Alex Leute <acl2809@rit.edu>
2025-01-15 21:17:01 -0500

    Added the ability to get the table of users from the database

--- api/app.js
+++ api/app.js
@@ -1,21 +1,22 @@
+"use strict";
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+const users = require("./routes/users");
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use("/api", users);
+
 // catch 404 and forward to error handler
-app.use(function(_, _, next) {
+app.use(function(_req, _res, next) {
   next(createError(404));
 });
 
---
+++ api/routes/users/index.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const router = express.Router();
+const mysql = require("mysql2");
+const fs = require("node:fs");
+const { exit } = require("node:process");
+console.log("loaded index")
+const password = (() => {
+    let password = "";
+    try {
+        password = fs.readFileSync("./database_password.txt", "utf8");
+        // Probably don't intend to have whitespace at the beginning or end of
+        // a password:
+        password = password.trim();
+    } catch (error) {
+        console.error(error);
+        console.error(
+            "Create a file called database_password.txt in the api directory,\n" +
+            "and paste your database password there"
+        )
+        exit(1);
+    }
+    return password;
+})();
+
+const pool = mysql.createPool({
+    host: 'localhost', 
+    user: 'root',      
+    password: password, 
+    database: 'BoardGame', 
+    connectionLimit: 10   
+});
+
+router.get("/users", (req, res) => {
+    pool.query('SELECT * FROM Users', (err, results) => {
+        if (err) {
+            console.error('Error fetching users:', err);
+            return res.status(500).send('Error fetching users');
+        }
+        res.json(results);
+    });
+});
+
+module.exports = router;
