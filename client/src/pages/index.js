c0800cb07d63e6c60a606007d4ab946503415e5a
nate02 <n8opot8oW@gmail.com>
2025-01-22 17:46:05 -0500

    Add pages directory

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,5 @@
+import { UserSignIn } from "../components/UserSignIn";
+import { UserSignup } from "../components/UserSignup";
 import { Search } from "./Search";
 
-export { Search };
+export { Search, UserSignIn, UserSignup };
