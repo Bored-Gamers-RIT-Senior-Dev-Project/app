be3edbb0ac163527e3d8a9cdf8ba366b98a49f2b
Alex Leute <acl2809@rit.edu>
2025-01-22 19:12:18 -0500

    Added a test for posting, that just returns the json you give it

---
+++ api/routes/test/index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const router = express.Router();
+
+router.post("/test", (req, res) => {
+    console.log(req.body);
+    return res.status(200).send(JSON.stringify(req.body, undefined, 4));
+});
+
+module.exports = router;
