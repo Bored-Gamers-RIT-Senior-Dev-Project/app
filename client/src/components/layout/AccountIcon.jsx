aaab1c495a9fa2a6d70b81cae1b405ace4ac24c3
Nathaniel Bachelder <nrw4912@rit.edu>
2025-01-23 16:14:29 -0500

    Replace user icon w/ sign in button

--- client/src/components/layout/AccountIcon.jsx
+++ client/src/components/layout/AccountIcon.jsx
@@ -1,47 +1,18 @@
-import { AccountCircle } from "@mui/icons-material";
-import { Box, Button, IconButton, Menu, MenuItem } from "@mui/material";
-import { useState } from "react";
+import { Box, Button } from "@mui/material";
 import { useNavigate } from "react-router";
 const AccountIcon = () => {
   //TODO: If user is logged in, display their profile picture and change the menu.
-  const [anchor, setAnchor] = useState(null);
-  const handleClick = (event) => {
-    setAnchor(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchor(null);
-  };
-
   const navigate = useNavigate();
-  const handleNavigate = (path) => {
-    handleClose();
-    navigate(path);
-  };
 
   return (
     <Box>
       <Button
         variant="contained"
-        size="small"
         color="secondary"
         onClick={() => navigate("/signin")}
       >
         Sign In
       </Button>
-      <Menu
-        id="account-menu"
-        anchorEl={anchor}
-        open={Boolean(anchor)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={() => handleNavigate("/login")}>Log In</MenuItem>
-        <MenuItem onClick={() => handleNavigate("/register")}>
-          Register
-        </MenuItem>
-      </Menu>
-      <IconButton color="inherit" onClick={handleClick}>
-        <AccountCircle />
-      </IconButton>
     </Box>
   );
 };
