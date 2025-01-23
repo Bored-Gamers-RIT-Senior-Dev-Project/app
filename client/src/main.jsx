cb93ba6663191b92f2b88767b7c5284ce2b03c41
Nathaniel Bachelder <nrw4912@rit.edu>
2025-01-23 17:09:11 -0500

    Make Router a js file again

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import "./index.css";
-import { router } from "./utils/router.jsx";
+import { router } from "./utils/router.js";
 import { theme } from "./utils/theme.js";
 
 createRoot(document.getElementById("root")).render(
