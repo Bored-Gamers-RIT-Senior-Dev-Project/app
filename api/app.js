c4686319190c6e1579f7bacfb72e41e5ac6640b9
Nathaniel Bachelder <nrw4912@rit.edu>
2025-01-28 15:23:19 -0500

    functioning test on Search page

--- api/app.js
+++ api/app.js
@@ -1,42 +1,42 @@
 // Adding for now to ban dumb things durning development, but we can remove
 // this later:
 "use strict";
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const users = require("./routes/users");
 const test = require("./routes/test");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cookieParser());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use("/api", users);
 app.use("/api", test);
 
-
 // catch 404 and forward to error handler
-app.use(function(_req, _res, next) {
+app.use(function (_req, _res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, _) {
+app.use(function (err, req, res, _) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   res.json({
     message: err.message, // https://stackoverflow.com/a/32836884
-    error: err
+    error: err,
   }); // FIXME: This is temporary, send to user friendly error page instead
 });
 
--- api/routes/test/index.js
+++ api/routes/test/index.js
@@ -1,9 +1,9 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 
 router.post("/test", (req, res) => {
-    console.log(req.body);
-    return res.status(200).send(JSON.stringify(req.body, undefined, 4));
+  console.log(req.body);
+  return res.status(200).send(JSON.stringify(req.body, undefined, 4));
 });
 
 module.exports = router;
