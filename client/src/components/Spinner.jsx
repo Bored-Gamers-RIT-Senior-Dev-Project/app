4e7f2b0ac963922d553262462f32e31751c73da0
nate02 <n8opot8oW@gmail.com>
2025-01-31 16:43:04 -0500

    Add useEffect to spinner subscription

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -1,11 +1,14 @@
 import { Backdrop, CircularProgress } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { events } from "../utils/events";
 
 const Spinner = () => {
   const [open, setOpen] = useState(false);
-  events.subscribe("spinner.open", () => setOpen(true));
-  events.subscribe("spinner.close", () => setOpen(false));
+  useEffect(() => {
+    events.subscribe("spinner.open", () => setOpen(true));
+    events.subscribe("spinner.close", () => setOpen(false));
+  }, []);
+
   return (
     <Backdrop open={open} sx={{ zIndex: 10000 }}>
       <CircularProgress color="primary" />
