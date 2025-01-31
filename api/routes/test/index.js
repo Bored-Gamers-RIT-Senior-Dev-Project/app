952599b05179c18e628a9e1b85a1ac695a129545
nate02 <n8opot8oW@gmail.com>
2025-01-31 15:47:25 -0500

    Add more to the testing endpoint

--- api/routes/test/index.js
+++ api/routes/test/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/test", (req, res) => {
-  console.log(req.body);
-  return res.status(200).send(JSON.stringify(req.body, undefined, 4));
+router.post("/test", async (req, res) => {
+  await new Promise((resolve) => {
+    setTimeout(resolve, req.body?.sleep || 3000);
+  });
+  console.log("Received the following from the client:", req.body);
+  return res
+    .status(200)
+    .send(JSON.stringify({ ...req.body, served: true }, undefined, 4));
 });
 
 module.exports = router;
