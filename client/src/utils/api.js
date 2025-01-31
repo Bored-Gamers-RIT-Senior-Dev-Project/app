67a7b7ac921fba4f598e6c1dd9910370ef3e3c25
nate02 <n8opot8oW@gmail.com>
2025-01-31 17:04:22 -0500

    Message + api demo v1

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,14 @@
 import axios from "axios";
+import { events } from "./events";
 const API_URL = "http://localhost:3000/api";
 
 const url = (path) => `${API_URL}/${path}`;
 
-const sendTest = async (data) => axios.post(url("test"), data);
+const sendTest = async (params) => {
+  let { data } = await axios.post(url("test"), params);
+  console.log("Axios: ", data);
+  events.publish("message", { message: "Axios: " + JSON.stringify(data) });
+  return data;
+};
 const getTest = async () => axios.get(url("users"));
 export { getTest, sendTest };
