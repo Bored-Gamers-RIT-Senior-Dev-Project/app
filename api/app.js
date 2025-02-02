1d80720f6222a827c87a6e0b519f67317965ffea
Raghav Sharma <rs2178@g.rit.edu>
2025-02-02 15:05:21 -0500

    Did the edits.

--- api/app.js
+++ api/app.js
@@ -1,12 +1,28 @@
+"use strict";
+
 const express = require("express");
-const cors = require("./middleware/cors");
+const createError = require("http-errors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const users = require("./routes/users");
 const authRoutes = require("./routes/index");
+const test = require("./routes/test");
+const auth = require("./routes/auth");
 
 const app = express();
 
 // Middleware
-app.use(cors);
+app.use(logger("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors()); // Enable CORS
+app.use(bodyParser.json());
 
 // Debugging Middleware (Logs all incoming requests)
 app.use((req, res, next) => {
@@ -14,11 +30,24 @@ app.use((req, res, next) => {
   next();
 });
 
-
+// Routes
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+app.use("/api/test", test);
 app.use("/api/routes", authRoutes);
 
-// Start the server
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+
+app.use((req, res, next) => {
+  next(createError(404));
 });
+
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err : {}, 
+  });
+});
+
+module.exports = app;
