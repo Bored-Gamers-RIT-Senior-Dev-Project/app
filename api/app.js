b33470a03b90691bf1926b56079da6b8c241998d
Raghav Sharma <rs2178@g.rit.edu>
2025-02-03 13:27:41 -0500

    update

--- api/app.js
+++ api/app.js
@@ -32,24 +32,6 @@ app.use("/api/users", users);
 //app.use("/api", test);
 //app.use("/api", auth);
 
-// catch 404 and forward to error handler
-app.use((_req, _res, next) => {
-  next(createError(404));
-});
-
-// error handler
-app.use((err, req, res, _) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: err,
-  });
-});
 
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
