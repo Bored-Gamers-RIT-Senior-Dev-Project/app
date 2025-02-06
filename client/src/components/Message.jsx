e8cc5cf7eddf3194a547c0cfc85dc43efa1416e8
Nathaniel Bachelder <nrw4912@rit.edu>
2025-02-06 09:33:57 -0500

    Message component updated for potential Titles

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,19 +1,22 @@
-import { Alert, Snackbar } from "@mui/material";
+import { Alert, AlertTitle, Snackbar } from "@mui/material";
 import { useEffect, useState } from "react";
 import { events } from "../utils/events";
 
 const defaultMessageSettings = {
+  title: "",
   message: "",
   severity: "info",
-  icon: () => <></>,
+  color: null,
+  icon: null,
   autoHideDuration: null,
 };
 
 const Message = () => {
   const [open, setOpen] = useState(false);
-  const [{ message, severity, icon, autoHideDuration }, setMessage] = useState(
-    defaultMessageSettings
-  );
+  const [
+    { message, severity, color, icon, autoHideDuration, title },
+    setMessage,
+  ] = useState(defaultMessageSettings);
   const handleClose = () => {
     setOpen(false);
   };
@@ -31,13 +34,16 @@ const Message = () => {
       onClose={handleClose}
       autoHideDuration={autoHideDuration}
       key={message}
+      anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
     >
       <Alert
         onClose={handleClose}
         severity={severity}
+        color={color}
         variant="filled"
         icon={icon}
       >
+        {title && <AlertTitle>{title}</AlertTitle>}
         {message}
       </Alert>
     </Snackbar>
