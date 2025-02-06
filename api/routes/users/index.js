812002c02077954a48a7d8e651ad320ff76875a4
Nathaniel Bachelder <nrw4912@rit.edu>
2025-02-06 13:28:38 -0500

    Functional db-side SignIn code (Again, requires a role in the DB with id=1 to work.)

--- api/routes/users/index.js
+++ api/routes/users/index.js
@@ -1,24 +1,67 @@
 const express = require("express");
 const router = express.Router();
 const admin = require("firebase-admin");
+const db = require("../../config/db");
 const { verifyFirebaseToken } = require("../../services/firebase");
 
 router.post("/signin", async (req, res) => {
-  const { idToken } = req.body;
-  try {
-    const decodedToken = await verifyFirebaseToken(idToken);
+    const { idToken, method } = req.body;
+    if (!idToken || !method) {
+        return res.status(400).json({ message: "Invalid request format." });
+    }
 
-    res.status(200).json({
-      message: "Sign-in successful!",
-      user: {
-        uid: decodedToken.uid,
-        email: decodedToken.email,
-      },
-    });
-  } catch (error) {
-    console.error("Error during sign-in:", error.message);
-    res.status(401).json({ message: "Invalid ID token", error: error.message });
-  }
+    //Get UID from the firebase token
+    let uid;
+    try {
+        const userEntry = await verifyFirebaseToken(idToken);
+        uid = userEntry.uid;
+    } catch {
+        return res.status(401).json({ message: "Invalid Firebase Token" });
+    }
+
+    try {
+        let dbUser = await db.query(
+            "SELECT Username, Email, RoleID, ProfileImageURL, TeamID, UniversityID FROM Users WHERE FirebaseUID = ?",
+            [uid]
+        );
+
+        //Email/Password Sign in logic.
+        if (method === "email") {
+            if (dbUser[0].length === 0) {
+                //If user doesn't exist in the DB.  This shouldn't happen if the method is email.
+                console.log("WARNING: User in Firebase not found in Database.");
+                return res.status(404).json({ message: "User not found" });
+            }
+        }
+        //Google sign-in logic
+        else if (method === "google") {
+            console.log("Google sign-in logic");
+            const { email, displayName, photoURL } = req.body;
+            //If this is the google user's first log-in, they haven't been loaded into the local DB yet.
+            if (dbUser[0].length === 0) {
+                await db.query(
+                    "INSERT INTO Users (FirebaseUID, Email, Username, ProfileImageUrl, RoleId) VALUES (?, ?, ?, ?, ?)",
+                    [uid, email, displayName, photoURL, 1]
+                );
+                dbUser = await db.query(
+                    "SELECT Username, Email, RoleID, ProfileImageURL, TeamID, UniversityID FROM Users WHERE FirebaseUID = ?",
+                    [uid]
+                );
+            }
+        }
+
+        //Successful sign-in.  Report to user and send the dbUser information for the frontend to store
+        res.status(200).json({
+            message: "Sign-in successful!",
+            user: dbUser[0][0],
+        });
+    } catch (error) {
+        console.error("Error during sign-in:", error.message);
+        res.status(401).json({
+            message: "Invalid ID token",
+            error: error.message,
+        });
+    }
 });
 
 router.post("/signup", async (req, res) => {
@@ -47,17 +90,17 @@ router.post("/signup", async (req, res) => {
             "SELECT Username, Email, RoleID, ProfileImageURL, TeamID, UniversityID FROM Users WHERE FirebaseUID = ?",
             [uid]
         );
-    res.status(201).json({
-      message: "Welcome!",
+        res.status(201).json({
+            message: "Welcome!",
             user: userQuery[0][0],
-    });
-  } catch (error) {
+        });
+    } catch (error) {
         await admin.auth().deleteUser(uid);
-    console.error("Error during sign-up:", error.message);
+        console.error("Error during sign-up:", error.message);
         res.status(500).json({
             message: "Sign-up failed",
             error: error.message,
         });
-  }
+    }
 });
 module.exports = router;
--- client/src/pages/UserSignIn.jsx
+++ client/src/pages/UserSignIn.jsx
@@ -8,126 +8,134 @@ import { events } from "../utils/events";
 import { signInWithEmail, signInWithGoogle } from "../utils/firebase/auth";
 
 const handleErrors = (error) => {
-  switch (error.message) {
-    default:
-      console.error("Sign-in error:", error);
-      events.publish("message", {
-        message: "An unexpected error occurred",
-        severity: "error",
-      });
-  }
+    switch (error.message) {
+        default:
+            console.error("Sign-in error:", error);
+            events.publish("message", {
+                message: "An unexpected error occurred",
+                severity: "error",
+            });
+    }
 };
 
 const UserSignIn = () => {
-  const [signInData, setSignInData] = useState({ email: "", password: "" });
-  const actionData = useActionData();
-  const submit = usePostSubmit();
-  const navigate = useNavigate();
+    const [signInData, setSignInData] = useState({ email: "", password: "" });
+    const actionData = useActionData();
+    const submit = usePostSubmit();
+    const navigate = useNavigate();
 
-  const handleSignInChange = (e) => {
-    setSignInData({ ...signInData, [e.target.name]: e.target.value });
-  };
+    const handleSignInChange = (e) => {
+        setSignInData({ ...signInData, [e.target.name]: e.target.value });
+    };
 
-  const handleSignInSubmit = async (e) => {
-    e.preventDefault();
-    events.publish("spinner.open");
-    try {
-      const user = await signInWithEmail(signInData.email, signInData.password);
-      const idToken = await user.getIdToken();
-      submit({ idToken, method: "email" });
-    } catch (error) {
-      handleErrors(error);
-      events.publish("spinner.close");
-    }
-    // events.publish("spinner.close");
-  };
+    const handleSignInSubmit = async (e) => {
+        e.preventDefault();
+        events.publish("spinner.open");
+        try {
+            const user = await signInWithEmail(
+                signInData.email,
+                signInData.password
+            );
+            const idToken = await user.getIdToken();
+            submit({ idToken, method: "email" });
+        } catch (error) {
+            handleErrors(error);
+            events.publish("spinner.close");
+        }
+        // events.publish("spinner.close");
+    };
 
-  const handleGoogleSignIn = async () => {
-    events.publish("spinner.open");
-    try {
-      const user = await signInWithGoogle();
-      const idToken = await user.getIdToken();
-      const { displayName, photoUrl, email } = user;
-      submit({ idToken, displayName, photoUrl, email, method: "google" });
-    } catch (error) {
-      //TODO: If the user is new and an error took place in the API, we need to handle that case and erase the user from Firebase.
-      handleErrors(error);
-      events.publish("spinner.close");
-    }
-  };
+    const handleGoogleSignIn = async () => {
+        events.publish("spinner.open");
+        try {
+            const user = await signInWithGoogle();
+            const idToken = await user.getIdToken();
+            console.log(user);
+            const { displayName, photoURL, email } = user;
+            submit({ idToken, displayName, photoURL, email, method: "google" });
+        } catch (error) {
+            handleErrors(error);
+            events.publish("spinner.close");
+        }
+    };
 
-  //ActionData is the response from the usePostSubmit action, defined in router.jsx
-  //Will be undefined until the action is complete.  Then, we can use it to handle the result.
-  useEffect(() => {
-    if (actionData) {
-      events.publish("spinner.close");
-      events.publish("message", { message: actionData.message });
-      if (actionData.message === "Signin successful") {
-        navigate("/"); // Redirect to home on successful sign-in
-      }
-    }
-  }, [actionData, navigate]);
-  return (
-    <Box
-      sx={{
-        maxWidth: "500px",
-        margin: "auto",
-        padding: "20px",
-        display: "flex",
-        flexDirection: "column",
-        gap: "20px",
-        backgroundColor: "white",
-        borderRadius: "10px",
-        boxShadow: "0px 4px 10px rgba(0,0,0,0.1)",
-      }}
-    >
-      <Typography variant="h4" textAlign="center">
-        User Sign In
-      </Typography>
-      <form onSubmit={handleSignInSubmit}>
-        <TextField
-          label="Email"
-          name="email"
-          type="email"
-          fullWidth
-          margin="normal"
-          value={signInData.email}
-          onChange={handleSignInChange}
-          required
-        />
-        <TextField
-          label="Password"
-          name="password"
-          type="password"
-          fullWidth
-          margin="normal"
-          value={signInData.password}
-          onChange={handleSignInChange}
-          required
-        />
-        <Button type="submit" variant="contained" color="primary" fullWidth>
-          Sign In
-        </Button>
-      </form>
-      <Button
-        variant="contained"
-        color="secondary"
-        fullWidth
-        onClick={handleGoogleSignIn}
-        startIcon={<Google />}
-      >
-        Sign in with Google
-      </Button>
-      {/*TODO: "Forgot Password/Forgot Username" link*/}
-      {/* TODO: Sign up link should look more like a link rather than plaintext  */}
-      <Typography
-        sx={{ mt: 2, textAlign: "center", cursor: "pointer" }}
-        onClick={() => navigate("/signup")}
-      >
-        No account? Sign Up
-      </Typography>
-    </Box>
-  );
+    //ActionData is the response from the usePostSubmit action, defined in router.jsx
+    //Will be undefined until the action is complete.  Then, we can use it to handle the result.
+    useEffect(() => {
+        if (actionData) {
+            events.publish("spinner.close");
+            events.publish("message", { message: actionData.message });
+            if (actionData.message === "Signin successful") {
+                navigate("/"); // Redirect to home on successful sign-in
+            }
+        }
+    }, [actionData, navigate]);
+    return (
+        <Box
+            sx={{
+                maxWidth: "500px",
+                margin: "auto",
+                padding: "20px",
+                display: "flex",
+                flexDirection: "column",
+                gap: "20px",
+                backgroundColor: "white",
+                borderRadius: "10px",
+                boxShadow: "0px 4px 10px rgba(0,0,0,0.1)",
+            }}
+        >
+            <Typography variant="h4" textAlign="center">
+                User Sign In
+            </Typography>
+            <form onSubmit={handleSignInSubmit}>
+                <TextField
+                    label="Email"
+                    name="email"
+                    type="email"
+                    fullWidth
+                    margin="normal"
+                    value={signInData.email}
+                    onChange={handleSignInChange}
+                    required
+                />
+                <TextField
+                    label="Password"
+                    name="password"
+                    type="password"
+                    fullWidth
+                    margin="normal"
+                    value={signInData.password}
+                    onChange={handleSignInChange}
+                    required
+                />
+                <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                >
+                    Sign In
+                </Button>
+            </form>
+            <Button
+                variant="contained"
+                color="secondary"
+                fullWidth
+                onClick={handleGoogleSignIn}
+                startIcon={<Google />}
+            >
+                Sign in with Google
+            </Button>
+            {/*TODO: "Forgot Password/Forgot Username" link*/}
+            {/* TODO: Sign up link should look more like a link rather than plaintext  */}
+            <Typography
+                sx={{ mt: 2, textAlign: "center", cursor: "pointer" }}
+                onClick={() => navigate("/signup")}
+            >
+                No account? Sign Up
+            </Typography>
+        </Box>
+    );
 };
 
 export { UserSignIn };
