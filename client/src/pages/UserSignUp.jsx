3e9f6ab51821d568f96789bd40421d4feb147de9
Alex Leute <alex@alexleute.com>
2025-02-10 01:11:19 -0500

    Password suggestions done!

--- client/src/pages/UserSignUp.jsx
+++ client/src/pages/UserSignUp.jsx
@@ -14,11 +14,16 @@ import { useActionData, useNavigate } from "react-router";
 import { usePostSubmit } from "../hooks/usePostSubmit";
 import { events } from "../utils/events";
 import { signUpWithEmail } from "../utils/firebase/auth";
+import PropTypes from "prop-types";
 
 // From https://zxcvbn-ts.github.io/zxcvbn/guide/framework-examples/#react:
-import { ZxcvbnFactory } from "@zxcvbn-ts/core";
+import { zxcvbnOptions, zxcvbnAsync } from "@zxcvbn-ts/core";
+/**
+ * @import { ZxcvbnResult, Score } from "@zxcvbn-ts/core"
+ */
 import * as zxcvbnCommonPackage from "@zxcvbn-ts/language-common";
 import * as zxcvbnEnPackage from "@zxcvbn-ts/language-en";
+import { translations } from "@zxcvbn-ts/language-en";
 
 /**
  * @class ErrorData a convenience for representing the data needed for an error
@@ -77,13 +82,13 @@ const options = {
     useLevenshteinDistance: true,
 };
 
-const zxcvbn = new ZxcvbnFactory(options);
+zxcvbnOptions.setOptions(options);
 
 /**
  * Score the given password
- * From https://github.com/zxcvbn-ts/zxcvbn/tree/master/docs/guide/framework-examples
+ * From https://zxcvbn-ts.github.io/zxcvbn/guide/framework-examples/#react
  * @param {string} password The password to score
- * @returns zxcvbn's password scoring
+ * @returns {ZxcvbnResult} zxcvbn's password scoring
  */
 const usePasswordStrength = (password) => {
     const [result, setResult] = useState(null);
@@ -91,32 +96,92 @@ const usePasswordStrength = (password) => {
     const deferredPassword = useDeferredValue(password);
 
     useEffect(() => {
-        zxcvbn
-            .checkAsync(deferredPassword)
-            .then((response) => setResult(response));
+        zxcvbnAsync(deferredPassword).then((response) => setResult(response));
     }, [deferredPassword]);
 
     return result;
 };
+/**
+ * Based on the score, return an adjective describing that score
+ * @param {Score} score the score of the password
+ * @returns {string} an adjective: one of "Excellent", "Good", "OK" or "Bad"
+ */
+const passwordAdjective = (score) => {
+    switch (score) {
+        case 0:
+        case 1:
+            return "Bad";
+        case 2:
+            return "OK";
+        case 3:
+            return "Good";
+        case 4:
+            return "Excellent";
+    }
+};
+/**
+ * Given a zxcvbn score, return a color
+ * @param {Score} score the score of the password
+ * @returns {string} A color
+ */
+const passwordColor = (score) => {
+    // https://coolors.co/ad1a24-5d5a0e-386141
+    switch (score) {
+        case 0:
+        case 1:
+            return "#AD1A24";
+        case 2:
+            // Yeah, not very many yellows work against a white background
+            return "#5D5A0E";
+        case 3:
+        case 4:
+            return "#386141";
+    }
+};
+/**
+ * @typedef {Object} SignUpData
+ * @property {string} email the email
+ * @property {string} username the username
+ * @property {string} password the password
+ * @property {string} repeatPassword should be the same as the password, but might not be
+ */
 
 /**
  * A component for the password bar
+ * @param {Object} params
+ * @param {SignUpData} params.signUpData
  * @returns the password strength component
  */
-const PasswordStrength = (signUpData) => {
+const PasswordStrength = ({ signUpData }) => {
+    const result = usePasswordStrength(signUpData.password) ?? {
+        score: 0,
+    };
+    const score = result.score;
+    let reason = "";
+    if (result.feedback && result.feedback.suggestions) {
+        for (const item of result.feedback.suggestions) {
+            reason += translations.suggestions[item] + " ";
+        }
+    }
+    const color = passwordColor(score);
+    const adjective = passwordAdjective(score);
     return (
-        <Box sx={{ width: "100%", color: "red" }}>
+        <Box sx={{ width: "100%", color: color }}>
             <Typography color="inherit">
-                <b></b>
+                Password strength: <b>{adjective}</b>
             </Typography>
             <LinearProgress
                 variant="determinate"
-                value={50}
+                value={(score / 4) * 100}
                 color="inherit"
             ></LinearProgress>
-            <Typography color="inherit">D</Typography>
+            <Typography color="inherit">{reason}</Typography>
         </Box>
     );
+};
+
+PasswordStrength.propTypes = {
+    signUpData: PropTypes.object,
 };
 
 const UserSignUp = () => {
