27cef773c1754e40781da309f4e098141ab83e15
Nathaniel Bachelder <nrw4912@rit.edu>
2025-02-13 18:56:57 -0500

    Comment out useless (?) files to see if anything breaks.

--- api/middlewares/authMiddleware.js
+++ api/middlewares/authMiddleware.js
@@ -1,22 +1,23 @@
-const jwt = require("jsonwebtoken");
+//FIXME: Useless file?
+// const jwt = require("jsonwebtoken");
 
-exports.authenticateToken = (req, res, next) => {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
+// exports.authenticateToken = (req, res, next) => {
+//     const authHeader = req.headers["authorization"];
+//     const token = authHeader && authHeader.split(" ")[1];
 
-    if (!token) {
-        return res
-            .status(401)
-            .json({ message: "Access Denied: No Token Provided" });
-    }
+//     if (!token) {
+//         return res
+//             .status(401)
+//             .json({ message: "Access Denied: No Token Provided" });
+//     }
 
-    jwt.verify(token, "your_secret_key", (err, user) => {
-        // FIXME  "your_secret_key" should probably be something else...
-        if (err) {
-            return res.status(403).json({ message: "Invalid Token" });
-        }
+//     jwt.verify(token, "your_secret_key", (err, user) => {
+//         // FIXME  "your_secret_key" should probably be something else...
+//         if (err) {
+//             return res.status(403).json({ message: "Invalid Token" });
+//         }
 
-        req.user = user; // Attach user information to request
-        next();
-    });
-};
+//         req.user = user; // Attach user information to request
+//         next();
+//     });
+// };
