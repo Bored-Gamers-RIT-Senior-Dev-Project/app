e2c6f0c5c6bc8b576c2cda642e4dd41b2223cddb
Nathaniel Bachelder <nrw4912@rit.edu>
2025-02-13 17:34:30 -0500

    Implement Users Model in Users Controller

--- api/routes/users/index.js
+++ api/routes/users/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const admin = require("firebase-admin");
 const db = require("../../config/db");
 const { verifyFirebaseToken } = require("../../config/firebase");
+const { createUser, getUser } = require("../../models/user");
 
 router.post("/signin", async (req, res) => {
     const { idToken, method } = req.body;
@@ -10,56 +11,35 @@ router.post("/signin", async (req, res) => {
         return res.status(400).json({ message: "Invalid request format." });
     }
 
-    //Get UID from the firebase token
-    let uid;
     try {
-        const userEntry = await verifyFirebaseToken(idToken);
-        uid = userEntry.uid;
-    } catch {
-        return res.status(401).json({ message: "Invalid Firebase Token" });
-    }
+        const firebaseUser = await verifyFirebaseToken(idToken);
+        let user = await getUser(firebaseUser.uid);
+        if (!user) {
+            if (method === "google") {
+                const { email, displayName, photoURL } = req.body;
 
-    try {
-        let dbUser = await db.query(
-            "SELECT Username, Email, RoleID, ProfileImageURL, TeamID, UniversityID FROM Users WHERE FirebaseUID = ?",
-            [uid]
-        );
-
-        //Email/Password Sign in logic.
-        if (method === "email") {
-            if (dbUser[0].length === 0) {
-                //If user doesn't exist in the DB.  This shouldn't happen if the method is email.
-                console.log("WARNING: User in Firebase not found in Database.");
-                return res.status(404).json({ message: "User not found" });
+                const username = email.split("@")[0];
+                const names = displayName.split(" ");
+                const firstName = names[0];
+                const lastName = names.slice(1).join(" ");
+                const output = await createUser(
+                    idToken,
+                    email,
+                    firstName,
+                    lastName,
+                    username,
+                    photoURL
+                );
+                console.log(output);
+                user = await getUser(firebaseUser.uid);
+            } else {
+                return res.status(401).json({ message: "User not found" });
             }
         }
-        //Google sign-in logic
-        else if (method === "google") {
-            const { email, displayName, photoURL } = req.body;
-
-            const username = email.split("@")[0];
-            const names = displayName.split(" ");
-            const firstName = names[0];
-            const lastName = names.slice(1).join(" ");
-
-            //If this is the google user's first log-in, they haven't been loaded into the local DB yet.
-            if (dbUser[0].length === 0) {
-                await db.query(
-                    "INSERT INTO Users (FirebaseUID, Email, Username, FirstName, LastName, ProfileImageUrl, RoleId) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                    [uid, email, username, firstName, lastName, photoURL, 1]
-                );
-                dbUser = await db.query(
-                    "SELECT Username, Email, RoleID, ProfileImageURL, TeamID, UniversityID FROM Users WHERE FirebaseUID = ?",
-                    [uid]
-                );
-            }
-        }
-
-        //Successful sign-in.  Report to user and send the dbUser information for the frontend to store
-        res.status(200).json({
+        return res.status(200).json({
             message: "Sign-in successful!",
-            user: dbUser[0][0],
-        });
+            user: user,
+        }); //Successful sign-in.  Report to user and send the dbUser information for the frontend to store
     } catch (error) {
         console.error("Error during sign-in:", error.message);
         res.status(401).json({
@@ -87,17 +67,21 @@ router.post("/signup", async (req, res) => {
 
     //Create the user in the local database
     try {
-        await db.query(
-            "INSERT INTO Users (FirstName, LastName, Username, Email, FirebaseUID, RoleId) VALUES (?, ?, ?, ?, ?, ?)",
-            [firstName, lastName, username, email, uid, 1]
+        const firebaseUser = await verifyFirebaseToken(idToken);
+        const result = await createUser(
+            firebaseUser.uid,
+            email,
+            firstName,
+            lastName,
+            username
         );
-        const userQuery = await db.query(
-            "SELECT Username, Email, RoleID, ProfileImageURL, TeamID, UniversityID FROM Users WHERE FirebaseUID = ?",
-            [uid]
-        );
+        console.log("Success: ", result);
+
+        const user = await getUserByUid(firebaseUser.uid);
+
         res.status(201).json({
             message: "Welcome!",
-            user: userQuery[0][0],
+            user: user,
         });
     } catch (error) {
         await admin.auth().deleteUser(uid);
