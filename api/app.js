9613f8800364a47f5ae9f885de4e9d2c0693577e
nate02 <n8opot8oW@gmail.com>
2025-02-14 00:41:53 -0500

    Remove bodyparser redundancies

--- api/app.js
+++ api/app.js
@@ -6,7 +6,6 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const users = require("./routes/users");
@@ -20,8 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors()); // Enable CORS
-app.use(bodyParser.json());
-
 app.use("/api/users", users);
 app.use("/api", test);
 
