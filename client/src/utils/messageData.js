2a008b9a2dff5300a5f91f6a4fd4559d24ef4de0
Alex Leute <acl2809@rit.edu>
2025-02-14 17:24:17 -0500

    Add a more general class MessageData

---
+++ client/src/utils/messageData.js
@@ -0,0 +1,41 @@
+/**
+ * An enum for Severity
+ */
+export const Severity = Object.freeze({
+    INFO: "info",
+    ERROR: "error",
+    WARNING: "warning",
+    SUCCESS: "success",
+});
+
+/**
+ * @class MessageData a convenience for representing the data needed for a message
+ */
+export class MessageData {
+    /**
+     * Create a new MessageData
+     * @param {string | undefined} title The title of this message
+     * @param {string} message The user-facing message for this message
+     * @param {string} severity The Severity
+     */
+    constructor(title, message, severity) {
+        this.title = title;
+        this.message = message;
+        this.severity = severity;
+    }
+}
+
+/**
+ * @class ErrorData a convenience for representing the data needed for an error
+ */
+export class ErrorData {
+    /**
+     * Create a new ErrorData
+     * @param {string} message The user-facing message for this error
+     * @param {string} [string] The Severity, or Severity.ERROR if not specified.
+     */
+    constructor(message, severity = Severity.ERROR) {
+        const messageData = new MessageData(undefined, message, severity);
+        Object.assign(this, messageData);
+    }
+}
