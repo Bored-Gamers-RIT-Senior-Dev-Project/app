fd2bab1211ddd7f50c1402e7b4a4d3cf1cc3641e
nate02 <n8opot8oW@gmail.com>
2025-02-16 15:43:38 -0500

    Introduce Context Provider for Authentication Data

---
+++ client/src/hooks/Auth/AuthProvider.jsx
@@ -0,0 +1,43 @@
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import { getUserData } from "../../utils/api";
+import { observeAuthState } from "../../utils/firebase/auth";
+import { AuthContext } from "./AuthContext";
+
+const AuthProvider = ({ children }) => {
+    const [token, setToken] = useState(null);
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        observeAuthState(async (firebaseUser) => {
+            if (firebaseUser) {
+                const token = await firebaseUser.getIdToken();
+                setToken(token);
+            } else {
+                setToken(null);
+            }
+        });
+    }, []);
+
+    useEffect(() => {
+        if (token) {
+            getUserData(token).then((data) => {
+                console.log(data);
+                setUserData(data);
+            });
+        }
+    }, [token]);
+
+    useEffect(() => {
+        console.log(userData);
+    }, [userData]);
+
+    return (
+        <AuthContext.Provider value={userData}>{children}</AuthContext.Provider>
+    );
+};
+AuthProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export { AuthProvider };
