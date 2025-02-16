4dfd3b094ebe3902e45fb15388d37fdee1610f67
nate02 <n8opot8oW@gmail.com>
2025-02-16 15:43:38 -0500

    Introduce Context Provider for Authentication Data

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,17 @@ import { ThemeProvider } from "@mui/material";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
+import { AuthProvider } from "./hooks/Auth/AuthProvider.jsx";
 import "./index.css";
 import { router } from "./utils/router.jsx";
 import { theme } from "./utils/theme.js";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <ThemeProvider theme={theme}>
-            <RouterProvider router={router} />
-        </ThemeProvider>
+        <AuthProvider>
+            <ThemeProvider theme={theme}>
+                <RouterProvider router={router} />
+            </ThemeProvider>
+        </AuthProvider>
     </StrictMode>
 );
