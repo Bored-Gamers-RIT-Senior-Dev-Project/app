8ef4d653b7b26cbf112685a45447d0e6ddba2f55
Joe Schultz <jxscad@rit.edu>
2025-02-16 17:30:50 -0500

    Create stub of functions needed in tournament model

---
+++ api/models/tournamentModel.js
@@ -0,0 +1,58 @@
+const db = require("../config/db");
+
+/**
+ * Creates a new tournament in the database.
+ */
+const createTournament = async () => {
+    // TODO: Implement tournament creation
+    throw new Error("Not implemented");
+};
+
+/**
+ * Read basic information about a tournament from the database by ID.
+ */
+const readTournament = async () => {
+    // TODO: Implement querying tournament info
+    throw new Error("Not implemented");
+};
+
+/**
+ * Updates the tournament state in the database.
+ */
+const updateTournament = async () => {
+    // TODO: Implement updating tournament state
+    throw new Error("Not implemented");
+};
+
+/**
+ * Creates a match for the tournament in the database.
+ */
+const createMatch = async () => {
+    // TODO: Implement match creation
+    throw new Error("Not implemented");
+};
+
+/**
+ * Sets the winner of a match in the database.
+ */
+const setMatchWinner = async () => {
+    // TODO: Implement settings match results
+    throw new Error("Not implemented");
+};
+
+/**
+ * Inserts new matches for the next round into the database.
+ */
+const insertNextRoundMatches = async () => {
+    // TODO: Implement adding matches to next of tournament
+    throw new Error("Not implemented");
+};
+
+module.exports = {
+    createTournament,
+    readTournament,
+    updateTournament,
+    createMatch,
+    setMatchWinner,
+    insertNextRoundMatches,
+};
