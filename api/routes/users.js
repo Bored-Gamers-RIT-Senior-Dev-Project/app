1a6ea44bfb69d1a50352720741d233f1a1b02696
Nathaniel Bachelder <nrw4912@rit.edu>
2025-02-18 13:41:15 -0500

    Remove unused camel case require

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const UserService = require("../services/userService");
 const createError = require("http-errors");
-const { makeObjectCamelCase } = require("../utils");
 
 router.post("/get", async (req, res, next) => {
     const { token } = req.body;
