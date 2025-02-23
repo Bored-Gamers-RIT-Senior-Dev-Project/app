2f13d62a433f4252d8a3c5049e9784d59412a1e6
Alex Leute <alex@alexleute.com>
2025-02-23 02:07:32 -0500

    Finished encodeImage...
    a function that encodes images to webp, and resizes them to a reasonable size

--- api/services/uploadService.js
+++ api/services/uploadService.js
@@ -1,10 +1,28 @@
+const sharp = require("sharp");
+
+// The maximum length of the longest side of a user-uploaded image, in pixels:
+const MAX_LONGEST_SIDE = 1000;
+
 /**
  * Encode image as WEBP, discarding any metadata
+ * Also, resize image so that it is at most 1000px on it's longest side
  * @param {Buffer} image an image to attempt to encode
- * @return {Buffer | null} The image, encoded as WEBP, or null if it couldn't be converted
+ * @return {Promise<Buffer | null>} The image, encoded as WEBP, or null if it couldn't be converted
  */
-const encodeImage = (image) => {
+const encodeImage = async (image) => {
+    try {
+        return await sharp(image)
+            .resize(MAX_LONGEST_SIDE, MAX_LONGEST_SIDE, {
+                fit: "inside",
+                withoutEnlargement: true
+            })
+            .webp()
+            .toBuffer()
 
+    } catch (e) {
+        console.warn(e);
+        return null;
+    }
 }
 
-module.exports = {}
+module.exports = { encodeImage };
