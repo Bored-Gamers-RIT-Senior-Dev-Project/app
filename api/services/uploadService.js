566bb73de5d0bfbca4ceecb7ac678f5a0350a544
Alex Leute <alex@alexleute.com>
2025-02-23 00:56:33 -0500

    Stub out encodeImage

---
+++ api/services/uploadService.js
@@ -0,0 +1,10 @@
+/**
+ * Encode image as WEBP, discarding any metadata
+ * @param {Buffer} image an image to attempt to encode
+ * @return {Buffer | null} The image, encoded as WEBP, or null if it couldn't be converted
+ */
+const encodeImage = (image) => {
+
+}
+
+module.exports = {}
