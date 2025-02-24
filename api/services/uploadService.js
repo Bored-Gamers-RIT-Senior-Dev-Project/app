187f2095d5c76270c63ed396fb6cad02e9a952f8
Alex Leute <alex@alexleute.com>
2025-02-24 01:04:49 -0500

    Add comments, and stub out saveImage

--- api/services/uploadService.js
+++ api/services/uploadService.js
@@ -1,9 +1,11 @@
 const sharp = require("sharp");
 const { createHash } = require("node:crypto");
+const fs = require("node:fs/promises")
 
 // The maximum length of the longest side of a user-uploaded image, in pixels:
 const MAX_LONGEST_SIDE = 1000;
-
+// The directory to where images are stored
+const USER_IMAGE_DIRECTORY = __dirname + "../user-images"
 /**
  * Encode image as WEBP, discarding any metadata
  * Also, resize image so that it is at most 1000px on it's longest side
@@ -26,7 +28,8 @@ const encodeImage = async (image) => {
 };
 
 /**
- * Hash a buffer with sha1
+ * Hash a buffer with sha1 (not for cryptographic purposes, intended just to
+ * tell if two files are the same).
  * @param {Buffer} buffer
  * @returns {string} sha1 of the buffer, base64url encoded
  */
@@ -37,3 +40,11 @@ const hash = (buffer) => {
 };
 
 module.exports = { encodeImage, hash };
+
+/**
+ * Save an image to the disk
+ * @param {string} name the name of the image
+ * @param {Buffer} image a buffer containing the image data
+ */
+const saveImage = async (name, image) => {
+}
