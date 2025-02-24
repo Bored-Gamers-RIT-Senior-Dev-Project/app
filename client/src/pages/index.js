c1a5b5fe61c77084045c8b0f5050042df39de1bc
Nathaniel Bachelder <nrw4912@rit.edu>
2025-02-24 11:39:10 -0500

    Fix missing imports/exports in index.js after my earlier fix

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,16 +1,20 @@
+import { Home } from "./Home";
 import { NotFound } from "./NotFound";
 import { Rules } from "./Rules";
 import { Schedule } from "./Schedule";
 import { Search } from "./Search";
 import { University } from "./University";
+import { UserSettings } from "./UserSettings";
 import { UserSignIn } from "./UserSignIn";
 import { UserSignUp } from "./UserSignUp";
 export {
+    Home,
     NotFound,
     Rules,
     Schedule,
     Search,
     University,
+    UserSettings,
     UserSignIn,
     UserSignUp,
 };
