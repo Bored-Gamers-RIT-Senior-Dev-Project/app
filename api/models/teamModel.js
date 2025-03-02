f07b8d807113c9c21b56b0591a53262b966cf3d3
nate02 <n8opot8oW@gmail.com>
2025-03-02 14:00:09 -0500

    Implement teamService

---
+++ api/models/teamModel.js
@@ -0,0 +1,45 @@
+const db = require("../config/db");
+
+/**
+ * Searches for teams based on the search term.
+ *
+ * @param {string} teamName - The team name to search for.
+ * @param {string} universityName - the University Name to search for.  Default to null.
+ * @param {boolean} partial - If partial matches should be included.  Defaults to true.
+ * @param {boolean} approvedOnly - If only approved entries should be listed in the result.  Defaults to true.
+ * @returns {Promise<Array>} - A promise that resolves to an array of search results.
+ */
+const searchTeams = async (
+    teamName,
+    universityName = null,
+    partial = true,
+    approvedOnly = true
+) => {
+    //TODO create teamModel and move SQL interactions there
+    let sql = `
+        SELECT 
+            t.TeamID AS Id, 
+            t.TeamName,
+            t.ProfileImageURL,
+            t.Description,
+            u.UniversityName,
+            'Team' AS Type
+        FROM Teams t
+        JOIN Universities u ON t.UniversityID = u.UniversityID
+        WHERE 
+            (t.TeamName LIKE ?`;
+    const fieldPacket = [partial ? `%${teamName}%` : teamName];
+    if (universityName) {
+        fieldPacket.push(partial ? `%${universityName}%` : universityName);
+        sql += ` OR u.UniversityName LIKE ?)`;
+    } else sql += ")";
+    if (approvedOnly) {
+        sql += " AND t.IsApproved = true";
+    }
+
+    const teamQuery = await db.query(sql, fieldPacket);
+
+    return teamQuery[0];
+};
+
+module.exports = { searchTeams };
