2d8ef4f8967fd303df05d09bf76d2421dee56d69
nate02 <n8opot8oW@gmail.com>
2025-03-02 14:28:03 -0500

    Create university routes page

---
+++ api/routes/university.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const { makeObjectCamelCase } = require("../utils");
+const universityService = require("../services/universityService");
+
+router.get("/:universityId", async (req, res, next) => {
+    try {
+        const universityInfo = await universityService.getUniversityInfo(
+            req.params.universityId
+        );
+        return res.json(makeObjectCamelCase(universityInfo));
+    } catch (e) {
+        next(e);
+    }
+});
