641e031fc6a34e081187520482f34fa0ed7924de
nate02 <n8opot8oW@gmail.com>
2025-03-02 12:27:56 -0500

    Rudimentary implementation of search

--- client/src/utils/router.jsx
+++ client/src/utils/router.jsx
@@ -49,6 +49,7 @@ const router = createBrowserRouter([
                 path: "/search",
                 element: <Search />,
                 action: makeAction(search),
+                loader: () => search({ value: "" }),
             },
             {
                 path: "/university/:universityId",
