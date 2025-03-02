aeb8f6b6e5656afb572a74cac3653a74590f7d63
nate02 <n8opot8oW@gmail.com>
2025-03-02 00:48:15 -0500

    Add filter dropdown

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -3,31 +3,21 @@ import { useState } from "react";
 
 import {
     Box,
+    FormControl,
+    Grid2 as Grid,
     InputAdornment,
+    InputLabel,
     Paper,
     TextField,
     Typography,
 } from "@mui/material";
-// const universityList = [
-//     "Rochester Institute of Technology",
-//     "Harvard University",
-//     "MIT",
-//     "Stanford University",
-// ];
 
-// const cityList = [
-//     "Rochester",
-//     "New York City",
-//     "Los Angeles",
-//     "Chicago",
-//     "Houston",
-//     "Phoenix",
-// ];
+import { DynamicSelect } from "../components/DynamicSelect";
 
 const Search = () => {
     // const [selectedUniversity, setSelectedUniversity] = useState(null);
     // const [selectedCity, setSelectedCity] = useState(null);
-    // const [sorting, setSorting] = useState("");
+    const [sorting, setSorting] = useState("alphabetical");
     const [searchBar, setSearchBar] = useState("");
 
     return (
@@ -35,33 +25,61 @@ const Search = () => {
             sx={{
                 display: "flex",
                 flexDirection: "column",
-                width: "800px",
-                maxWidth: "95%",
+                width: {
+                    xs: "90vw",
+                    md: "800px",
+                },
+                // maxWidth: "95vw",
             }}
         >
             <Paper component="div" sx={{ padding: { xs: 1, md: 3 } }}>
                 <Typography variant="h4" textAlign="center" sx={{ pb: 3 }}>
                     Search
                 </Typography>
-
-                <TextField
-                    label="Search"
-                    variant="outlined"
-                    color="primary"
-                    name="search"
-                    slotProps={{
-                        input: {
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <SearchIcon />
-                                </InputAdornment>
-                            ),
-                        },
-                    }}
-                    value={searchBar}
-                    onChange={(_e, value) => setSearchBar(value)}
-                    sx={{ width: "100%" }}
-                />
+                <Grid container spacing={1}>
+                    <Grid size={{ xs: 12, md: 9 }}>
+                        <TextField
+                            label="Search"
+                            variant="outlined"
+                            color="primary"
+                            name="search"
+                            slotProps={{
+                                input: {
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <SearchIcon />
+                                        </InputAdornment>
+                                    ),
+                                },
+                            }}
+                            value={searchBar}
+                            onChange={(_e, value) => setSearchBar(value)}
+                            sx={{ width: "100%" }}
+                        />
+                    </Grid>
+                    <Grid size={{ xs: 6, md: 3 }}>
+                        <FormControl fullWidth>
+                            <InputLabel htmlFor="sortBy" id="sortByLabel">
+                                Sort By
+                            </InputLabel>
+                            <DynamicSelect
+                                fullWidth
+                                id="sortBy"
+                                labelId="sortByLabel"
+                                label="Sort By"
+                                variant="outlined"
+                                value={sorting}
+                                onChange={({ target }) =>
+                                    setSorting(target.value)
+                                }
+                                options={{
+                                    alphabetical: "Alphabetical",
+                                    universityName: "University Name",
+                                }}
+                            />
+                        </FormControl>
+                    </Grid>
+                </Grid>
                 {/* <Accordion>
                     <AccordionSummary expandIcon={<ExpandMore />}>
                         Advanced Search
