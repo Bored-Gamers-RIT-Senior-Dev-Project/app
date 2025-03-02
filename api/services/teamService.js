cdd7dea379c757ef586cd48a9e2c9edce5665ccc
nate02 <n8opot8oW@gmail.com>
2025-03-02 11:45:34 -0500

    Fix service issues with sql (don't code at 2am, kids)

--- api/services/teamService.js
+++ api/services/teamService.js
@@ -3,27 +3,43 @@ const db = require("../config/db");
 /**
  * Searches for teams based on the search term.
  *
- * @param {string} teamName - The term to search for.
+ * @param {string} teamName - The team name to search for.
+ * @param {string} universityName - the University Name to search for.  Default to null.
  * @param {boolean} partial - If partial matches should be included.  Defaults to true.
+ * @param {boolean} approvedOnly - If only approved entries should be listed in the result.  Defaults to true.
  * @returns {Promise<Array>} - A promise that resolves to an array of search results.
  */
-const searchTeams = async (teamName, partial = true) => {
+const searchTeams = async (
+    teamName,
+    universityName = null,
+    partial = true,
+    approvedOnly = true
+) => {
     //TODO create teamModel and move SQL interactions there
-    const sql = `
+    let sql = `
         SELECT 
-            t.TeamID AS ID, 
-            t.TeamName AS Name, 
-            'Team' AS Type,
-            u.UniversityName AS AssociatedUniversity
-        FROM Team t
-        JOIN University u ON t.UniversityID = u.UniversityID
+            t.TeamID, 
+            t.TeamName,
+            t.ProfileImageURL,
+            t.Description,
+            u.UniversityName,
+            'Team' AS Type
+        FROM Teams t
+        JOIN Universities u ON t.UniversityID = u.UniversityID
         WHERE 
-            t.TeamName ILIKE ? 
-            OR u.UniversityName ILIKE ?`;
-    const teamQuery = await db.query(sql, [
-        partial ? `%${teamName}%` : teamName,
-    ]);
-    teamQuery[0];
+            (t.TeamName LIKE ?`;
+    const fieldPacket = [partial ? `%${teamName}%` : teamName];
+    if (universityName) {
+        fieldPacket.push(partial ? `%${universityName}%` : universityName);
+        sql += ` OR u.UniversityName LIKE ?)`;
+    } else sql += ")";
+    if (approvedOnly) {
+        sql += " AND t.IsApproved = true";
+    }
+
+    const teamQuery = await db.query(sql, fieldPacket);
+
+    return teamQuery[0];
 };
 
 module.exports = { searchTeams };
