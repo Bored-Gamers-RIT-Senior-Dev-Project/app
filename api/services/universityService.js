536ccff6b84fd90991f6cd9398b89208e04326ac
nate02 <n8opot8oW@gmail.com>
2025-03-02 12:27:46 -0500

    Small updates to the format of returned search data

--- api/services/universityService.js
+++ api/services/universityService.js
@@ -7,11 +7,25 @@ const createHttpError = require("http-errors");
  * @param {string} universityName - The search term for the university name.
  * @returns {Promise<Array>} - A promise that resolves to an array of search results.
  */
-const searchUniversities = async (universityName) => {
-    const searchResult = await universityModel.searchUniversities(
-        universityName
-    );
-    return searchResult;
+const searchUniversities = async (universityName, partial = true) => {
+    //TODO: Create universityModel and move sql logic there.
+    let sql = `
+        SELECT 
+            UniversityId AS Id,
+            UniversityName, 
+            Location,
+            LogoURL,
+            Description,
+            WebsiteURL,
+            'University' AS Type
+        FROM Universities
+        WHERE 
+            UniversityName LIKE ?`;
+
+    const query = await db.query(sql, [
+        partial ? `%${universityName}%` : universityName,
+    ]);
+    return query[0];
 };
 
 const getUniversityInfo = async (universityId) => {
