a977d976efdfe84d845db24da36e8b481f444d03
nate02 <n8opot8oW@gmail.com>
2025-03-01 23:14:05 -0500

    Comment out the advanced search functionalities until I decide how I want to deal with them.

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,39 +1,34 @@
 import { useState } from "react";
 
-import { ExpandMore, Search as SearchIcon } from "@mui/icons-material";
+import { Search as SearchIcon } from "@mui/icons-material";
 
 import {
-    Accordion,
-    AccordionDetails,
-    AccordionSummary,
-    Autocomplete,
     Box,
-    Grid2 as Grid,
     InputAdornment,
     Paper,
     TextField,
     Typography,
 } from "@mui/material";
-const universityList = [
-    "Rochester Institute of Technology",
-    "Harvard University",
-    "MIT",
-    "Stanford University",
-];
+// const universityList = [
+//     "Rochester Institute of Technology",
+//     "Harvard University",
+//     "MIT",
+//     "Stanford University",
+// ];
 
-const cityList = [
-    "Rochester",
-    "New York City",
-    "Los Angeles",
-    "Chicago",
-    "Houston",
-    "Phoenix",
-];
+// const cityList = [
+//     "Rochester",
+//     "New York City",
+//     "Los Angeles",
+//     "Chicago",
+//     "Houston",
+//     "Phoenix",
+// ];
 
 const Search = () => {
-    const [selectedUniversity, setSelectedUniversity] = useState(null);
-    const [selectedCity, setSelectedCity] = useState(null);
-    const [sorting, setSorting] = useState("");
+    // const [selectedUniversity, setSelectedUniversity] = useState(null);
+    // const [selectedCity, setSelectedCity] = useState(null);
+    // const [sorting, setSorting] = useState("");
     const [searchBar, setSearchBar] = useState("");
 
     return (
@@ -68,7 +63,7 @@ const Search = () => {
                     onChange={(_e, value) => setSearchBar(value)}
                     sx={{ width: "100%" }}
                 />
-                <Accordion>
+                {/* <Accordion>
                     <AccordionSummary expandIcon={<ExpandMore />}>
                         Advanced Search
                     </AccordionSummary>
@@ -89,7 +84,7 @@ const Search = () => {
                                     )}
                                 />
                             </Grid>
-                            {/* <Grid size={{ xs: 12, md: 6 }}>
+                            <Grid size={{ xs: 12, md: 6 }}>
                                 <Autocomplete
                                     options={cityList}
                                     value={selectedCity}
@@ -103,11 +98,11 @@ const Search = () => {
                                         />
                                     )}
                                 />
-                            </Grid> */}
+                            </Grid>
                             <Grid size={{ xs: 12, md: 6 }}></Grid>
                         </Grid>
                     </AccordionDetails>
-                </Accordion>
+                </Accordion> */}
             </Paper>
         </Box>
     );
