8324760040a89ff76c6f93bc962b868154710fd5
nate02 <n8opot8oW@gmail.com>
2025-03-02 02:06:15 -0500

    Begin set up for search back-end

--- api/services/teamService.js
+++ api/services/teamService.js
@@ -1,16 +1,29 @@
-const teamModel = require("../models/teamModel");
+const db = require("../config/db");
 
 /**
  * Searches for teams based on the search term.
  *
- * @param {string} teamName - The team name to search for.
- * @param {string} universityName - the University Name to search for.  Default to null.
+ * @param {string} teamName - The term to search for.
+ * @param {boolean} partial - If partial matches should be included.  Defaults to true.
  * @returns {Promise<Array>} - A promise that resolves to an array of search results.
  */
-const searchTeams = async (teamName, universityName = null) => {
+const searchTeams = async (teamName, partial = true) => {
     //TODO create teamModel and move SQL interactions there
-    const results = await teamModel.searchTeams(teamName, universityName);
-    return results;
+    const sql = `
+        SELECT 
+            t.TeamID AS ID, 
+            t.TeamName AS Name, 
+            'Team' AS Type,
+            u.UniversityName AS AssociatedUniversity
+        FROM Team t
+        JOIN University u ON t.UniversityID = u.UniversityID
+        WHERE 
+            t.TeamName ILIKE ? 
+            OR u.UniversityName ILIKE ?`;
+    const teamQuery = await db.query(sql, [
+        partial ? `%${teamName}%` : teamName,
+    ]);
+    teamQuery[0];
 };
 
 module.exports = { searchTeams };
