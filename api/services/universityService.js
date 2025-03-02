a7aac0bb8cd88dfa9b8c86dd8da305ba45b20103
nate02 <n8opot8oW@gmail.com>
2025-03-02 13:59:15 -0500

    Make UniversityService work with UniversityModel

--- api/services/universityService.js
+++ api/services/universityService.js
@@ -1,16 +1,13 @@
-const db = require("../config/db");
 const universityModel = require("../models/universityModel");
 /**
  * Searches universities based on the search term.
  *
  * @param {string} universityName - The search term for the university name.
- * @param {boolean} partial - If the search should include partial matches
  * @returns {Promise<Array>} - A promise that resolves to an array of search results.
  */
 const searchUniversities = async (universityName) => {
     const searchResult = await universityModel.searchUniversities(
-        universityModel,
-        true
+        universityName
     );
     return searchResult;
 };
