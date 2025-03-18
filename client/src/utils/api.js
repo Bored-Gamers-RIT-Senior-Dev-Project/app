ff54579a4dc241dec0be3c598ba6da94ef8e4c6e
Nathaniel Bachelder <nrw4912@rit.edu>
2025-03-18 17:29:42 -0400

    Complete refactor of the front-end API code

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { events } from "./events";
 import { getIdToken } from "./firebase/auth";
 
 const API_URL = "http://localhost:3000/api";
@@ -23,6 +22,12 @@ const addAuthorization = async (config) => {
 };
 
 const api = axios.create();
+/**
+ * Decorates an axios HTTP function to add the authorization header and build the URL.
+ * @param {*} f The Express HTTP function that is being decorated.  This variant does not expect a body, and should be used for functions like GET and DELETE that don't have one.
+ * @returns A decorated version of the function provided that adds the authorization header and builds the URL.
+ * @example api.get = decorateRequest(api.get);
+ */
 const decorateRequest =
     (f) =>
     async (path, config = {}) => {
@@ -30,6 +35,13 @@ const decorateRequest =
         config = await addAuthorization(config);
         return await f(path, config);
     };
+
+/**
+ * Decorates an axios HTTP function to add the authorization header and build the URL.
+ * @param {*} f The Express HTTP function that is being decorated.  This variant expects a body, and should be used for functions like POST and PUT.
+ * @returns A decorated version of the function provided that adds the authorization header and builds the URL.
+ * @example api.post = decorateRequest(api.post);
+ */
 const decorateRequestWithBody =
     (f) =>
     async (path, body, config = {}) => {
@@ -42,24 +54,6 @@ api.put = decorateRequestWithBody(api.put);
 api.get = decorateRequest(api.get);
 api.delete = decorateRequest(api.delete);
 
-//=========================== TEST ===========================
-/**
- * Server test function.
- * @param {*} params
- * @returns {Promise<*>} The same as params.
- */
-const sendTest = async (params) => {
-    let { data } = await api.post("test", params);
-    events.publish("message", { message: "Axios: " + JSON.stringify(data) });
-    return data;
-};
-/**
- * Server test function.
- * @returns {Promise<*>} A list of users.
- */
-const getTest = async () => api.get("users");
-
-//========================== SEARCH ==========================
 /**
  *
  * @param {*} params
@@ -71,33 +65,67 @@ const search = async (params) => {
 };
 
 const users = {
+    /**
+     * Gets the user profile for the currently logged in user.
+     * @returns The user profile for the currently logged in user.
+     */
     getProfile: async () => {
         let { data } = await api.get("users/profile");
         return data;
     },
-    google: async (params) => {
-        let { data } = await api.post("users/google", params);
+    /**
+     * Processes a google sign-in to construct a Google user if none exists.
+     * @param {object} params to build the google user: {email, username, firstName, lastName}
+     * @param {string} params.email The email of the user.
+     * @param {string} params.username The username of the user.
+     * @param {string} params.firstName The first name of the user.
+     * @param {string} params.lastName The last name of the user.
+     * @returns The user profile, either newly created or existing, of the currently logged in user.
+     */
+    google: async ({ email, username, firstName, lastName }) => {
+        let { data } = await api.post("users/google", {
+            email,
+            username,
+            firstName,
+            lastName,
+        });
         return data;
     },
+    /**
+     * Creates a new user in the local API database
+     * @param {object} params
+     * @param {string} params.email The email of the user.
+     * @param {string} params.username The username of the user.
+     * @param {string} params.firstName The first name of the user.
+     * @param {string} params.lastName The last name of the user.
+     * @returns {object} The newly created user and a welcome message.
+     */
     signUp: async (params) => {
         let { data } = await api.post("users", params);
         return data;
     },
+    /**
+     *
+     * Updates the user profile for the specified user
+     * @param {*} params The keys to update in the user profile.  //TODO: This function should be further locked down or specified to what the front-end can do once we have the dashboard
+     * @returns {Promise<*>} A confirmation of the update
+     */
     update: async (params) => {
         let { data } = await api.put("users", params);
         return data;
     },
 };
 
-// ======================== UNIVERSITY ========================
-/**
- * Gets a university's information by its ID.
- * @param {*} universityId The university id.
- * @returns {Promise<*>} The university's information.  Null if no university with the given ID exists.
- */
-const getUniversityInfo = async (universityId) => {
-    let { data } = await axios.get(`university/${universityId}`);
-    return data;
+const university = {
+    /**
+     * Gets the university information for the specified university
+     * @param {*} universityId
+     * @returns {Promise<*>} The university information for the specified university
+     */
+    getInfo: async (universityId) => {
+        let { data } = await api.get(`university/${universityId}`);
+        return data;
+    },
 };
 
-export { getTest, getUniversityInfo, search, sendTest, users };
+export { search, university, users };
