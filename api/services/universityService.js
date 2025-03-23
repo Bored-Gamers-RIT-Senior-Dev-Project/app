8877ac471bc814e05526aa561af4287d62408a4b
nate02 <n8opot8oW@gmail.com>
2025-03-23 03:04:17 -0400

    University Service functionality for update and update (wip)

--- api/services/universityService.js
+++ api/services/universityService.js
@@ -1,4 +1,5 @@
 const universityModel = require("../models/universityModel");
+const userModel = require("../models/userModel");
 const teamModel = require("../models/teamModel");
 const createHttpError = require("http-errors");
 /**
@@ -48,8 +49,24 @@ const createUniversity = async (
     description,
     websiteUrl
 ) => {
-    //TODO: Check uid role and throw 403 if not admin. (Only administrators can create universities)
-    //TODO: Create university in database and return its ID.
+    //Check uid role and throw 403 if not admin. (Only administrators can create universities)
+    const user = await userModel.getUserByFirebaseId(uid);
+    if (user?.role !== "Super Admin") {
+        throw createHttpError(403);
+    }
+
+    //Create university in database.
+    await universityModel.createUniversity(
+        universityName,
+        location,
+        logoURL,
+        bannerUrl,
+        description,
+        websiteUrl
+    );
+
+    //TODO: Figure out what to return here.
+
     return {};
 };
 
@@ -62,14 +79,29 @@ const deleteUniversity = async (universityId) => {
 
 const updateUniversity = async (uid, universityId, updateBody) => {
     //TODO: Confirm that User is an admin or the University Rep.  Otherwise throw 403.
+    const user = await userModel.getUserByFirebaseId(uid);
+    if (
+        user.role !== "Super Admin" &&
+        (user.role !== "University Admin" || user.universityId !== universityId)
+    ) {
+        throw createHttpError(403);
+    }
+
     //TODO: Validate and sanitize updateBody to prevent SQL injection or other attacks/errors.
-    //TODO: Update university in universityModel.
+
+    //Update university in universityModel.
+    const university = await universityModel.updateUniversity(
+        universityId,
+        updateBody
+    );
+
     //TODO: Return updated university.
-    return {};
+    return university;
 };
 
 module.exports = {
     createUniversity,
+    updateUniversity,
     getUniversities,
     searchUniversities,
     getUniversityInfo,
