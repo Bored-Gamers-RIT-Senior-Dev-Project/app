c60ef441ad55fa010206e70cf0b936df3936ef1f
Joe Schultz <jxscad@rit.edu>
2025-03-28 10:20:19 -0400

    Pull userModel for getUserByFirebaseId to validate roles

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,5 +1,6 @@
+const createHttpError = require("http-errors");
 const db = require("../config/db");
-const randomString = require("../config/randomString");
+const { makeObjectCamelCase } = require("../utils");
 
 /**
  * Creates a user in the database.
@@ -17,16 +18,17 @@ const createUser = async (
     firebaseUID,
     email,
     firstName,
-    lastName = "",
-    username = email.split("@")[0] + "-" + randomString(4),
+    lastName,
+    username,
     profileImageUrl = `https://ui-avatars.com/api/?name=${firstName}+${lastName}?background=edca82`,
-    roleId = 1 // Default role for new users is Spectator
+    roleId = 1, // Default role for new users is Spectator
+    universityId
 ) => {
     try {
         const result = await db.query(
             `INSERT INTO
-                Users (FirebaseUID, Email, Username, FirstName, LastName, ProfileImageUrl, RoleId)
-                VALUES (?, ?, ?, ?, ?, ?, ?)`,
+                users (FirebaseUID, Email, Username, FirstName, LastName, ProfileImageUrl, RoleId, UniversityId)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
             [
                 firebaseUID,
                 email,
@@ -35,18 +37,13 @@ const createUser = async (
                 lastName,
                 profileImageUrl,
                 roleId,
+                universityId,
             ]
         );
-        return {
-            id: result.insertId,
-            firebaseUID,
-            email,
-            firstName,
-            lastName,
-            username,
-            profileImageUrl,
-            roleId,
-        };
+        if (result[0].affectedRows === 0) {
+            throw new Error("User not created.");
+        }
+        return await getUserByFirebaseId(firebaseUID);
     } catch (error) {
         console.error("Error creating user:", error.message);
         throw error;
@@ -54,29 +51,191 @@ const createUser = async (
 };
 
 /**
- * Retrieve a user from the database by their Firebase UID.
- * @param {string} uid The firebase UID of the user.  Should be validated from a token.
- * @return {Promise<object>} The user object from the database.
- * @throws {Error} If the user is not found.
+ * Retrieve a user from the database, using the column and value specified.
+ * @param {*} column The column to use to match with value.  (TO PREVENT SQL INJECTION, SHOULD ALWAYS BE USED HARD-CODED)
+ * @param {*} value The value of {column} to search for.
+ * @returns A matching user, or null if no user is found.
  */
-const readUser = async (uid) => {
+const getUser = async (column, value) => {
     try {
         const [rows] = await db.query(
             `
-            SELECT * 
-            FROM Users
-            WHERE FirebaseUID = ?
+        SELECT user.*, uni.UniversityName, team.TeamName
+            FROM users AS user
+            LEFT JOIN universities AS uni ON user.UniversityID = uni.UniversityId
+            LEFT JOIN teams AS team ON user.TeamID = team.TeamId
+            JOIN roles AS role ON user.RoleId = role.RoleId
+            WHERE ${column} = ?;
         `,
-            [uid]
+            [value]
         );
         if (rows.length === 0) {
             return null;
         }
-        return rows[0];
+        return makeObjectCamelCase(rows[0]);
     } catch (error) {
         console.error("Error fetching user:", error.message);
         throw error;
     }
 };
 
-module.exports = { createUser, readUser };
+/**
+ * Retrieve a user from the database by their local userId value.
+ * @param {string} userId The local ID of a user
+ * @return {Promise<object>} The user object from the database.
+ * @throws {Error} If the user is not found.
+ */
+const getUserByUserId = async (userId) => getUser("user.userID", userId);
+
+/**
+ * Retrieve a user from the database by their Firebase UID.
+ * @param {string} uid The firebase UID of the user.  Should be validated from a token.
+ * @return {Promise<object>} The user object from the database.
+ * @throws {Error} If the user is not found.
+ */
+const getUserByFirebaseId = async (uid) => getUser("user.firebaseUID", uid);
+
+const VALID_KEYS = [
+    "USERID",
+    "FIRSTNAME",
+    "LASTNAME",
+    "USERNAME",
+    "EMAIL",
+    "FIREBASEUID",
+    "PROFILEIMAGEURL",
+    "BIO",
+    "CREATEDAT",
+    "PAID",
+    "TEAMID",
+    "ROLEID",
+    "UNIVERSITYID",
+    "ISVALIDATED",
+    "UNIVERSITYNAME",
+    "TEAMNAME",
+];
+const updateUser = async (firebaseUid, body) => {
+    if (body.username) body.username = await generateUsername(body.username);
+    const updates = [];
+    for (const key of Object.keys(body)) {
+        if (VALID_KEYS.includes(key.toUpperCase())) {
+            updates.push(`${key} = ?`);
+        } else {
+            console.error("Error fulfilling update request:", body);
+            console.error("Invalid key:", key);
+            throw createHttpError(400, "Invalid attempt to update user.");
+        }
+    }
+    const keys = Object.keys(body)
+        .map((key) => `${key} = ?`)
+        .join(", ");
+
+    const [rows] = await db.query(
+        `UPDATE users SET ${keys} WHERE FirebaseUID = ?`,
+        [...Object.values(body), firebaseUid]
+    );
+
+    if (rows.affectedRows === 0) {
+        throw new Error("User not updated.");
+    }
+
+    return await getUserByFirebaseId(firebaseUid);
+};
+
+/**
+ *
+ * @param {number} The userID of the user in the local database
+ * @returns true
+ * @throws 404 HttpError if deletion fails
+ */
+const deleteUser = async (userId) => {
+    //Get the user record from the db.
+    const user = await getUserByUserId(userId);
+    if (!user) {
+        throw createHttpError(404);
+    }
+
+    const result = await db.query(`DELETE FROM users WHERE userID = ?`, [
+        userId,
+    ]);
+
+    if (result[0].affectedRows === 0) {
+        throw createHttpError(404);
+    }
+
+    return user;
+};
+
+/**
+ * Generates a list of usernames that start with the given "base" username.
+ * @param {String} username The username to check for duplicates.
+ * @returns  {Promise<Array>} An array of usernames that start with the given string.
+ */
+const getSharedUsernames = async (username) => {
+    try {
+        //Select all usernames that start with the given username
+        const [rows] = await db.query(
+            `SELECT Username FROM users WHERE lower(Username) LIKE lower(?)`,
+            [username + "%"]
+        );
+        //Return those rows.
+        return rows.map((row) => row.Username);
+    } catch (error) {
+        console.error("Error checking username:", error.message);
+        throw error;
+    }
+};
+
+/**
+ * Generates a unique username based on the given "base" username.
+ * @param {String} username The "base" username to use.
+ * @returns {Promise<String>} A unique username.
+ */
+const generateUsername = async (username, sharedUsernames = null) => {
+    // Get all usernames that start with the given username
+    if (!sharedUsernames) sharedUsernames = await getSharedUsernames(username);
+
+    // If the username is not taken, return it
+    if (!sharedUsernames.includes(username)) {
+        return username;
+    }
+
+    // Otherwise, append a number to the username until we find a unique one
+    let i = 1;
+    while (sharedUsernames.includes(`${username}-${i}`)) {
+        i++;
+        if (i > 100)
+            console.error(
+                `Error reserving username ${username}.  An unreasonable number of usernames were found with that prefix.`
+            );
+        throw new Error(
+            "Error reserving a username based on the information provided."
+        );
+    }
+    return `${username}-${i}`;
+};
+
+/**
+ * Checks a username for availability and generates a unique username if necessary.
+ * @param {String} username The username to check.
+ * @param {Boolean} strict If true, the function will throw an error if the username is already taken.
+ * @returns {Promise<String>} A unique username.
+ * @throws {Error} If the username is taken and strict is true.
+ */
+const checkUsername = async (username, strict = false) => {
+    const sharedUsernames = await getSharedUsernames(username);
+    if (sharedUsernames.includes(username) && strict) {
+        throw new Error("Username is taken");
+    }
+    return await generateUsername(username, sharedUsernames);
+};
+
+const getPermissions = async (roleId) => {};
+
+module.exports = {
+    createUser,
+    getUserByUserId,
+    getUserByFirebaseId,
+    updateUser,
+    deleteUser,
+    checkUsername,
+};
