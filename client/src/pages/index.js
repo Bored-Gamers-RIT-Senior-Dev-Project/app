cd53a88355bfcce61d1d7dcdb5954dba9e27dce8
nate02 <n8opot8oW@gmail.com>
2025-04-04 11:56:28 -0400

    (Credit to ChatGPT for splitting the code for me) convert TeamsPage into Edit and View components

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,32 +1,31 @@
+import { About } from "./About";
+import { AdminDashboard } from "./AdminDashboard";
 import { Home } from "./Home";
 import { NotFound } from "./NotFound";
+import { ReportView } from "./ReportView";
 import { Rules } from "./Rules";
 import { Schedule } from "./Schedule";
 import { Search } from "./Search";
-import { About } from "./About";
+import { TeamPage } from "./TeamPage";
 import { University } from "./University";
-import { TeamsPage } from "./TeamsPage";
+import { UserManager } from "./UserManager";
 import { UserSettings } from "./UserSettings";
 import { UserSignIn } from "./UserSignIn";
 import { UserSignUp } from "./UserSignUp";
-import { ReportView } from "./ReportView";
-import { AdminDashboard } from "./AdminDashboard";
-import { UserManager } from "./UserManager";
 
 export {
+    About,
+    AdminDashboard,
     Home,
     NotFound,
+    ReportView,
     Rules,
     Schedule,
     Search,
-    About,
+    TeamPage,
     University,
-    TeamsPage,
+    UserManager,
     UserSettings,
     UserSignIn,
     UserSignUp,
-    ReportView,
-    AdminDashboard,
-    UserManager,
 };
-
