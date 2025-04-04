3d9ec1fc5b2cd9b7780cc82fc1a27e3edb50a9b8
nate02 <n8opot8oW@gmail.com>
2025-04-04 13:26:04 -0400

    Move the list of users to its own component and make it functional

--- client/src/pages/TeamPage/index.jsx
+++ client/src/pages/TeamPage/index.jsx
@@ -3,55 +3,47 @@ import { useLoaderData } from "react-router";
 import { TeamEditor } from "./TeamEditor";
 import { TeamView } from "./TeamView";
 
+import { Box, Paper } from "@mui/material";
+import { MemberList } from "./MemberList";
+
 const TeamPage = () => {
     const team = useLoaderData();
     const [editMode, setEditMode] = useState(false);
-    const [teamName, setTeamName] = useState(team.teamName);
-    const [teamSummary, setTeamSummary] = useState(team.description ?? "");
-    const [members, setMembers] = useState([
-        {
-            id: 1,
-            name: "User Name",
-            role: "Member",
-            blurb: "Lorem ipsum dolor asdf this is a user blurb.",
-        },
-        {
-            id: 2,
-            name: "User Name",
-            role: "Scientist",
-            blurb: "Fourth-year student studying engineering. Member of the e-sports team.",
-        },
-        {
-            id: 3,
-            name: "User Name",
-            role: "Writer",
-            blurb: "Third-year student double-majoring Creative Writing and Theater.",
-        },
-    ]);
 
-    const [tab, setTab] = useState(0);
-
-    return editMode ? (
-        <TeamEditor
-            teamName={teamName}
-            setTeamName={setTeamName}
-            teamSummary={teamSummary}
-            setTeamSummary={setTeamSummary}
-            members={members}
-            setMembers={setMembers}
-            tab={tab}
-            setTab={setTab}
-            exitEditMode={() => setEditMode(false)}
-        />
-    ) : (
-        <TeamView
-            teamName={teamName}
-            teamSummary={teamSummary}
-            members={members}
-            tab={tab}
-            setTab={setTab}
-            enterEditMode={() => setEditMode(true)}
-        />
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                padding: { xs: 2, md: 4 },
+            }}
+        >
+            <Paper
+                sx={{
+                    padding: { xs: 2, md: 4 },
+                    width: "100%",
+                    maxWidth: "1200px",
+                    margin: "auto",
+                }}
+            >
+                {editMode ? (
+                    <TeamEditor
+                        teamName={team.teamName}
+                        teamDescription={team.summary}
+                        exitEditMode={() => setEditMode(false)}
+                    />
+                ) : (
+                    <TeamView
+                        teamName={team.teamName}
+                        teamSummary={team.description}
+                        members={team.members}
+                        enterEditMode={() => setEditMode(true)}
+                    />
+                )}
+                <MemberList members={team.members} captainId={team.captainId} />
+            </Paper>
+        </Box>
     );
 };
 
