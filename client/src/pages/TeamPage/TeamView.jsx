9aeea90d0a6733419936681e50a849167c4e671a
nate02 <n8opot8oW@gmail.com>
2025-04-06 01:15:43 -0400

    Remove duplicate "edit" buttons and add ui to hide edit button when user is not team captain

--- client/src/pages/TeamPage/TeamView.jsx
+++ client/src/pages/TeamPage/TeamView.jsx
@@ -2,7 +2,13 @@ import { Edit as EditIcon } from "@mui/icons-material";
 import { Avatar, Box, IconButton, Typography } from "@mui/material";
 import PropTypes from "prop-types";
 
-const TeamView = ({ teamName, teamSummary, teamImage, enterEditMode }) => {
+const TeamView = ({
+    showEditButton,
+    teamName,
+    teamSummary,
+    teamImage,
+    enterEditMode,
+}) => {
     return (
         <Box
             sx={{
@@ -13,26 +19,23 @@ const TeamView = ({ teamName, teamSummary, teamImage, enterEditMode }) => {
                 gap: 1,
             }}
         >
-            <IconButton
-                onClick={enterEditMode}
-                sx={{ position: "absolute", right: 0 }}
-            >
-                <EditIcon sx={{ color: "blue" }} />
-            </IconButton>
-
+            {showEditButton && (
+                <IconButton
+                    onClick={enterEditMode}
+                    sx={{
+                        position: "absolute",
+                        right: 0,
+                    }}
+                >
+                    <EditIcon sx={{ color: "blue" }} />
+                </IconButton>
+            )}
             <Avatar
                 src={teamImage}
                 alt={teamName}
                 sx={{ height: "7em", width: "7em" }}
             />
             <Typography variant="h4">{teamName}</Typography>
-
-            <IconButton
-                onClick={enterEditMode}
-                sx={{ position: "absolute", right: 0 }}
-            >
-                <EditIcon sx={{ color: "blue" }} />
-            </IconButton>
 
             <Box sx={{ mb: 2 }}>
                 <Typography variant="body1">{teamSummary}</Typography>
@@ -43,6 +46,7 @@ const TeamView = ({ teamName, teamSummary, teamImage, enterEditMode }) => {
 
 //Credit for proptypes: Copilot generation
 TeamView.propTypes = {
+    showEditButton: PropTypes.bool.isRequired,
     teamName: PropTypes.string.isRequired,
     teamImage: PropTypes.string.isRequired,
     teamSummary: PropTypes.string.isRequired,
