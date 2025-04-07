f5ba5498c4cbef7696b4ea73e132e275fec3eafc
Joe Schultz <jxscad@rit.edu>
2025-04-06 23:46:00 -0400

    Cherry pick client api util from main

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getIdToken } from "./firebase/auth";
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = import.meta.env.API_URL || "http://localhost:3000/api";
 
 /**
  * Constructs the full API URL for a given path.
